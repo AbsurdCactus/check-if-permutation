4ef2cf0939fece3b2b7ff057b8d6fbee2e5384f4
AbsurdCactus <braydonreber@gmail.com>
2021-11-05 08:29:20 -0400

    procrastinating

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+from unittest.main import main
+
+
+a = "bob"
+b = "bbo"
+
 def permutation_checker():
-    return "jimmy"
+    uh = set(a)
+    k = set(b)
+    return a==b
+
+print(permutation_checker())
+
+if __name__ == '__main__':
+    main()
